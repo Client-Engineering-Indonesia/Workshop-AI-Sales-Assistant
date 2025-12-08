0cd7c62cda4692432232fe43c8525fb15c83bc6a
Raden Adhitya Ardiansyah Ramadhan <ardiansyah.raar@gmail.com>
2025-12-08 16:10:02 +0700

    Update import.sh

--- Lab 3 - Introduction to Agent Development Kit/import.sh
+++ Lab 3 - Introduction to Agent Development Kit/import.sh
@@ -1,14 +1,4 @@
-# read from .env
-# set -a
-# source .env
-# set +a
-
-# create connections
 orchestrate env activate agentic-inc-3-v2
-# orchestrate connections add --app-id playground_app
-# orchestrate connections configure -a playground_app --env draft --kind key_value --type team
-
-# orchestrate connections set-credentials -a playground_app --env draft -e LANGFLOW_API_KEY=$LANGFLOW_API_KEY -e LANGFLOW_FLOW_ID=$LANGFLOW_FLOW_ID -e LANGFLOW_BASE_URL=$LANGFLOW_BASE_URL
 
 # create tools
 orchestrate tools import -k python -f tools/StatsCalculator.py -r tools/requirements.txt
