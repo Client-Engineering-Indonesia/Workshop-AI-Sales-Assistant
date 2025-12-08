a6f1f85d58083005a805fb5750d0f2a6559ee3fe
Raden Adhitya Ardiansyah Ramadhan <ardiansyah.raar@gmail.com>
2025-12-08 13:30:25 +0700

    update ADK

--- Lab 3 - Introduction to Agent Development Kit/import.sh
+++ Lab 3 - Introduction to Agent Development Kit/import.sh
@@ -1,16 +1,24 @@
 # read from .env
-set -a
-source .env
-set +a
+# set -a
+# source .env
+# set +a
 
 # create connections
 orchestrate env activate agentic-inc-3-v2
-orchestrate connections add --app-id playground_app
-orchestrate connections configure -a playground_app --env draft --kind key_value --type team
+# orchestrate connections add --app-id playground_app
+# orchestrate connections configure -a playground_app --env draft --kind key_value --type team
+
 # orchestrate connections set-credentials -a playground_app --env draft -e LANGFLOW_API_KEY=$LANGFLOW_API_KEY -e LANGFLOW_FLOW_ID=$LANGFLOW_FLOW_ID -e LANGFLOW_BASE_URL=$LANGFLOW_BASE_URL
 
 # create tools
-orchestrate tools import -k python -f tools/greeting.py --app-id playground_app
+# orchestrate tools import -k python -f tools/greeting.py
+orchestrate tools import -k python -f tools/greeting.py -r tools/requirements.txt
+# orchestrate tools import -k python -f tools/greeting.py --app-id playground_app
+
+# create knowledge
+orchestrate knowledge-bases import -f knowledge/report_extracted.yaml
 
 # create agents
+# orchestrate agents import -f agents/greeter.yaml
 orchestrate agents import -f agents/greeter.yaml
+orchestrate agents import -f agents/rag.yaml
